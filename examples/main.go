707dbadabad57b388bbd83042bb9f08e9283bd23
my-Sakura <69021934+my-Sakura@users.noreply.github.com>
2021-01-11 17:29:48 +0800

    examples

--- examples/main.go
+++ examples/main.go
@@ -6,10 +6,16 @@ import (
 	"github.com/my-Sakura/go-sms-server/api"
 )
 
+//smsProvider: tianyan
+//appCode: xxx
+//templateCode:
 func main() {
-	client := api.NewClient("tianyan", "your_appCode", "your_templateCode")
-	err := client.Send("mobile", 6)
+	client := api.NewClient("dingxin", "4c576b3341754ec2a8db38e6f70c1da9", "TP1711063")
+	err := client.Send("15032371660", 6)
 	if err != nil {
 		log.Println(err)
 	}
 }
+
+//your_templateCode
+//your_appCode
