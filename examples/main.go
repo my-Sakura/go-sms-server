dbd36f538e918bba085ee8ae436a0a13dfaca481
my-Sakura <69021934+my-Sakura@users.noreply.github.com>
2021-01-11 17:47:34 +0800

    examples

--- examples/main.go
+++ examples/main.go
@@ -10,12 +10,9 @@ import (
 //appCode: xxx
 //templateCode:
 func main() {
-	client := api.NewClient("dingxin", "4c576b3341754ec2a8db38e6f70c1da9", "TP1711063")
+	client := api.NewClient("smsProvider", "your_templateCode", "your_appCode")
 	err := client.Send("15032371660", 6)
 	if err != nil {
 		log.Println(err)
 	}
 }
-
-//your_templateCode
-//your_appCode
